8a16618b8b63d52ed7132353f97f3034cad3a982
Thomas Rijpstra <thomas@fourlights.nl>
2023-04-13 12:20:31 +0200

    fix: use static imports to resolve critical dependency warnings

--- admin/index.tsx
+++ admin/index.tsx
@@ -22,25 +22,34 @@ export default {
     })
   },
 
+  async importTranslation(locale: string) {
+    let data
+
+    // NOTE: We use a static import to prevent warnings about dynamic imports in webpack et al.
+    try {
+      switch (locale) {
+        case 'en':
+          data = (await import('./translations/en.json')).default
+          break
+        // NOTE: Add more cases for other supported locales
+        default:
+          data = {}
+      }
+    } catch (error) {
+      data = {}
+    }
+
+    return {
+      data: prefixPluginTranslations(data, plugin.id),
+      locale,
+    }
+  },
+
   async registerTrads(app: any) {
     const { locales } = app
 
     const importedTrads = await Promise.all(
-      (locales as any[]).map((locale) => {
-        return import(`./translations/${locale}.json`)
-          .then(({ default: data }) => {
-            return {
-              data: prefixPluginTranslations(data, plugin.id),
-              locale,
-            }
-          })
-          .catch(() => {
-            return {
-              data: {},
-              locale,
-            }
-          })
-      }),
+      (locales as string[]).map((locale) => this.importTranslation(locale)),
     )
 
     return Promise.resolve(importedTrads)
