0625b0200e23adad3c0c403b7694ac34cc9b8b7c
Thomas Rijpstra <thomas@fourlights.nl>
2023-07-05 10:00:40 +0200

    chore: show credentials in playground login

--- playground/src/admin/app.tsx
+++ playground/src/admin/app.tsx
@@ -1,6 +1,13 @@
 export default {
   config: {
     notifications: { releases: false },
+    translations: {
+      en: {
+        'Auth.form.welcome.title': 'Deep-Copy playground',
+        'Auth.form.welcome.subtitle': 'playground@example.com // Test1234',
+        'Auth.form.email.placeholder': 'playground@example.com',
+      }
+    }
   },
   bootstrap(app) {},
 };
